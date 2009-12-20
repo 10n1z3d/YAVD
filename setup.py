86cfbf64fcfd4bf05584eaf6b21640c0729a39a1
10n1z3d <10n1z3d@w.cn>
2009-12-20 19:18:20 +0200

    commit

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ import yavd
 setup (
         name = 'YAVD',
         version = yavd.__version__,
-        description= 'Download and convert videos from Youtube and others.',
+        description= 'Download videos from Youtube and others.',
         author = '10n1z3d',
         author_email = '10n1z3d@w.cn',
         url = '',
         license = 'GPLv3',
         packages = ['YAVD'],
-        data_files = [('YAVD/', ['README', 'COPYING'])]
+        data_files = [('YAVD/', ['README', 'COPYING', 'TODO'])]
       )
